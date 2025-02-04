08f7f92039315811694f3ab545c16dd13e3a1520
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-02-04 14:19:18 +0500

    setting navbar showing user profile , logout  , redirecting to admin dashboard

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -1,14 +1,22 @@
-  "use client";
-import React, { useEffect } from "react";
+"use client";
+import React, { useEffect, useState, useRef } from "react";
 import companyLogo from "../../../src/assets/companyLogo.png";
 import { useGetUserQuery, useSignOutMutation } from "../../features/usersApi";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { FaRegAddressCard } from "react-icons/fa"; // Import card icon
 
 function Navbar() {
   const navigate = useNavigate();
   const { data: userData } = useGetUserQuery();
-
   const [signOut] = useSignOutMutation();
+  const [showDropDown, setShowDropDown] = useState(false);
+
+  const dropdownMenuRef = useRef(null);
+  const navbarCollapseRef = useRef(null);
+
+  const handleDropDown = () => {
+    setShowDropDown(!showDropDown); 
+  };
 
   const handleLogOut = async () => {
     try {
@@ -18,49 +26,41 @@ function Navbar() {
       console.log(e.message);
     }
   };
+
+  function handleDropdownMouseMove(event) {
+    if (dropdownMenuRef.current) {
+      dropdownMenuRef.current.classList.add("show");
+    }
+  }
+
+  function handleDropdownMouseLeave(event) {
+    if (dropdownMenuRef.current) {
+      dropdownMenuRef.current.classList.remove("show");
+    }
+  }
+
+  const handleNavigateToDashboard = () => {
+    navigate("/admin/dashboard");
+  };
+
   function handleScroll() {
     const bodyScroll = window.scrollY;
     const navbar = document.querySelector(".navbar");
-
     if (bodyScroll > 300) navbar.classList.add("nav-scroll");
     else navbar.classList.remove("nav-scroll");
   }
+
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  function handleDropdownMouseMove(event) {
-    const dropdownMenu = event.currentTarget.querySelector(".dropdown-menu");
-    if (dropdownMenu) {
-      dropdownMenu.classList.add("show");
-    }
-  }
-
-  function handleDropdownMouseLeave(event) {
-    const dropdownMenu = event.currentTarget.querySelector(".dropdown-menu");
-    if (dropdownMenu) {
-      dropdownMenu.classList.remove("show");
-    }
-  }
-
   function handleToggleNav() {
-    if (
-      document
-        .querySelector(".navbar .navbar-collapse")
-        .classList.contains("show")
-    ) {
-      document
-        .querySelector(".navbar .navbar-collapse")
-        .classList.remove("show");
-    } else if (
-      !document
-        .querySelector(".navbar .navbar-collapse")
-        .classList.contains("show")
-    ) {
-      document.querySelector(".navbar .navbar-collapse").classList.add("show");
+    if (navbarCollapseRef.current) {
+      navbarCollapseRef.current.classList.toggle("show");
     }
   }
+
   return (
     <nav className="navbar navbar-expand-lg bord blur">
       <div className="container o-hidden">
@@ -71,11 +71,6 @@ function Navbar() {
         <button
           className="navbar-toggler"
           type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
           onClick={handleToggleNav}
         >
           <span className="icon-bar">
@@ -83,257 +78,44 @@ function Navbar() {
           </span>
         </button>
 
-        <div
-          className="collapse navbar-collapse justify-content-center"
-          id="navbarSupportedContent"
-        >
+        <div ref={navbarCollapseRef} className="collapse navbar-collapse justify-content-center">
           <ul className="navbar-nav">
-            {/* <li
-              onMouseLeave={handleDropdownMouseLeave}
-              onMouseMove={handleDropdownMouseMove}
-              className="nav-item dropdown"
-            >
-              <a
-                className="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-                href="#"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                <span className="rolling-text">Demos</span>
-              </a>
-              <div className="dropdown-menu mega-menu">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-lg">
-                      <a className="item-img text-center" href="/home-main">
-                        <span className="img">
-                          <img src="/assets/imgs/menu/1.png" alt="" />
-                        </span>
-                        <span className="mt-15">Main Home</span>
-                      </a>
-                    </div>
-                    <div className="col-lg">
-                      <a
-                        className="item-img text-center"
-                        href="/home-modern-studio"
-                      >
-                        <span className="img">
-                          <img src="/assets/imgs/menu/2.png" alt="" />
-                        </span>
-                        <span className="mt-15">Modern Studio</span>
-                      </a>
-                    </div>
-                    <div className="col-lg">
-                      <a
-                        className="item-img text-center"
-                        href="/home-creative-agency"
-                      >
-                        <span className="img">
-                          <img src="/assets/imgs/menu/3.png" alt="" />
-                        </span>
-                        <span className="mt-15">Creative Agency</span>
-                      </a>
-                    </div>
-                    <div className="col-lg">
-                      <a
-                        className="item-img text-center"
-                        href="/home-digital-agency"
-                      >
-                        <span className="img">
-                          <img src="/assets/imgs/menu/4.png" alt="" />
-                        </span>
-                        <span className="mt-15">Digital Agency</span>
-                      </a>
-                    </div>
-                    <div className="col-lg">
-                      <a className="item-img text-center" href="/home-personal">
-                        <span className="img">
-                          <img src="/assets/imgs/menu/5.png" alt="" />
-                        </span>
-                        <span className="mt-15">Freelancer</span>
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </li> */}
             <li
               onMouseLeave={handleDropdownMouseLeave}
               onMouseMove={handleDropdownMouseMove}
               className="nav-item dropdown"
             >
-              <a
-                className="nav-link dropdown-toggle"
-                data-toggle="dropdown"
-                href="/"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
+              <a className="nav-link dropdown-toggle" href="/">
                 <span className="rolling-text">Home</span>
               </a>
-              <ul className="dropdown-menu">
+              <ul ref={dropdownMenuRef} className="dropdown-menu">
                 <li>
                   <a className="dropdown-item" href="/page-about">
                     About Us
                   </a>
                 </li>
-                {/* <li>
-                  <a className="dropdown-item" href="/page-services">
-                    Services
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/page-services-details">
-                    Services Details
-                  </a>
-                </li> */}
-                {/* <li>
-                  <a className="dropdown-item" href="/page-team">
-                    Our Team
-                  </a>
-                </li> */}
-                {/* <li>
-                  <a className="dropdown-item" href="/page-team-details">
-                    Team Details
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/page-contact">
-                    Contact Us
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/page-FAQ">
-                    FAQS
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="/page-404">
-                    Error 404
-                  </a>
-                </li> */}
               </ul>
             </li>
-            <li
-              onMouseLeave={handleDropdownMouseLeave}
-              onMouseMove={handleDropdownMouseMove}
-              className="nav-item dropdown"
-            >
-              <a
-                className="nav-link"
-                data-toggle="dropdown"
-                href="/services"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
+            <li className="nav-item">
+              <a className="nav-link" href="/services">
                 <span className="rolling-text">Services</span>
               </a>
             </li>
-            <li
-              onMouseLeave={handleDropdownMouseLeave}
-              onMouseMove={handleDropdownMouseMove}
-              className="nav-item dropdown"
-            >
-              <a
-                className="nav-link"
-                data-toggle="dropdown"
-                href="/portfolio"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
+            <li className="nav-item">
+              <a className="nav-link" href="/portfolio">
                 <span className="rolling-text">Portfolio</span>
               </a>
-              {/* <div className="dropdown-menu">
-                <a className="dropdown-item" href="/portfolio-gallery">
-                  Gallery
-                </a>
-                <a className="dropdown-item" href="/portfolio-creative">
-                  Portfolio Creative
-                </a>
-                <a
-                  className="dropdown-item"
-                  href="/portfolio-creative-carousel"
-                >
-                  Creative Carousel
-                </a>
-                <a className="dropdown-item" href="/portfolio-grid">
-                  Portfolio Grid
-                </a>
-                <a className="dropdown-item" href="/portfolio-masonry">
-                  Portfolio Masonry
-                </a>
-                <a className="dropdown-item" href="/project-details">
-                  Project Details
-                </a>
-              </div> */}
             </li>
-            <li
-              onMouseLeave={handleDropdownMouseLeave}
-              onMouseMove={handleDropdownMouseMove}
-              className="nav-item dropdown"
-            >
-              <a
-                className="nav-link"
-                data-toggle="dropdown"
-                href="/stock"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
+            <li className="nav-item">
+              <a className="nav-link" href="/stock">
                 <span className="rolling-text">Stock</span>
               </a>
             </li>
-            <li
-              onMouseLeave={handleDropdownMouseLeave}
-              onMouseMove={handleDropdownMouseMove}
-              className="nav-item dropdown"
-            >
-              <a
-                className="nav-link"
-                data-toggle="dropdown"
-                href="/blog-classic"
-                role="button"
-                // aria-haspopup="true"
-                // aria-expanded="false"
-              >
+            <li className="nav-item">
+              <a className="nav-link" href="/blog-classic">
                 <span className="rolling-text">Blogs</span>
               </a>
-              {/* <div className="dropdown-menu">
-                <a className="dropdown-item" href="/blog-classic">
-                  Blog Standerd
-                </a>
-                <a className="dropdown-item" href="/blog-grid-sidebar">
-                  Grid With Sidebar
-                </a>
-                <a className="dropdown-item" href="/blog-grid-3column">
-                  Grid Three Column
-                </a>
-                <a className="dropdown-item" href="/blog-details">
-                  Blog Details
-                </a>
-              </div> */}
             </li>
-            {/* <li
-              onMouseLeave={handleDropdownMouseLeave}
-              onMouseMove={handleDropdownMouseMove}
-              className="nav-item dropdown"
-            >
-              <a
-                className="nav-link"
-                data-toggle="dropdown"
-                href="/page-about"
-                role="button"
-                aria-haspopup="true"
-                aria-expanded="false"
-              >
-                <span className="rolling-text">About</span>
-              </a>
-            </li> */}
             <li className="nav-item">
               <a className="nav-link" href="/page-contact">
                 <span className="rolling-text">Contact Us</span>
@@ -341,18 +123,36 @@ function Navbar() {
             </li>
           </ul>
         </div>
+
         {userData?.user ? (
-          <div className="contact-button" onClick={() => handleLogOut()}>
-            <a className="butn butn-sm butn-bg main-colorbg radius-5">
-              <span className="text">Logout</span>
-            </a>
-          </div>
+          <>
+            <div className="contact-button position-relative" onClick={handleDropDown}>
+              <a className="butn butn-sm butn-bg main-colorbg d-flex justify-content-center cursor-pointer align-items-center" style={{ width: "40px", height: "40px", borderRadius: "100%" }}>
+                {userData?.user?.firstName ? userData.user.fullName.charAt(0).toUpperCase() : "k"}
+              </a>
+            </div>
+            {showDropDown && (
+              <div className="profileDropDown position-absolute show mt-3">
+                <ul>
+                  {userData?.user?.role === "admin" && (
+                    <li className="pb-2">
+                      <a onClick={handleNavigateToDashboard}>
+                        <FaRegAddressCard className=" me-2" /> Dashboard
+                      </a>
+                    </li>
+                  )}
+                  <li>
+                    <a onClick={handleLogOut}>
+                      <i className="fas fa-sign-out-alt"></i> Log Out
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </>
         ) : (
           <div className="contact-button">
-            <a
-              href="/log-in"
-              className="butn butn-sm butn-bg main-colorbg radius-5"
-            >
+            <a href="/log-in" className="butn butn-sm butn-bg main-colorbg radius-5">
               <span className="text">Sign In</span>
             </a>
           </div>
