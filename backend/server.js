1dfc72836bf523956454fe811cfa1fc03e95f598
Muhammad Shakeel <shakeel7521951@gmail.com>
2025-02-06 23:39:42 +0500

    allow cors from every one

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: "*",  // Allows requests from any origin
     credentials: true,
   })
 );
